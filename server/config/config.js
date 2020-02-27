e83e675d66bdaf388a76a780527380c3e275d91b
unknown <solo.nunca21@gmail.com>
2020-02-27 16:31:41 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,26 @@
-
-
 // =====================
 //   PUERTO
 // =====================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// =====================
+//   ENTORNO
+// =====================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; 
+
+// =====================
+//   BD
+// =====================
+
+let urlBD;
+
+if( process.env.NODE_ENV === 'dev') {
+      urlBD = 'mongodb://localhost:27017/cafe';
+}else{
+      urlBD = 'mongodb+srv://coffeedb:coffee789@cafe-573gh.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+process.env.URLBD = urlBD;
