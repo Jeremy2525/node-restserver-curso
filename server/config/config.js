0f06bc716afcbec5742e3241c4210139bf85718d
unknown <solo.nunca21@gmail.com>
2020-02-28 09:18:17 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlBD;
 if( process.env.NODE_ENV === 'dev') {
       urlBD = 'mongodb://localhost:27017/cafe';
 }else{
-      urlBD = 'mongodb+srv://coffeedb:coffee789@cafe-573gh.mongodb.net/test?retryWrites=true&w=majority';
+      urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLBD = urlBD;
